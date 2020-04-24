08f724ebc12385e37b1248713a4ffae1e40bd24e
风间琉璃 <1203183302@qq.com>
2020-04-24 13:53:04 +0800

    s

--- file-store.sql
+++ file-store.sql
@@ -1,59 +1,92 @@
-SET FOREIGN_KEY_CHECKS=0;
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : master
+ Source Server Type    : MySQL
+ Source Server Version : 50728
+ Source Host           : localhost:3306
+ Source Schema         : file-store
+
+ Target Server Type    : MySQL
+ Target Server Version : 50728
+ File Encoding         : 65001
+
+ Date: 24/04/2020 01:17:19
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `file_folder`
+-- Table structure for file_folder
 -- ----------------------------
 DROP TABLE IF EXISTS `file_folder`;
-CREATE TABLE `file_folder` (
+CREATE TABLE `file_folder`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文件夹ID',
-  `file_folder_name` varchar(255) DEFAULT NULL COMMENT '文件夹名称',
-  `parent_folder_id` int(11) DEFAULT '0' COMMENT '父文件夹ID',
-  `file_store_id` int(11) DEFAULT NULL COMMENT '所属文件仓库ID',
-  `time` datetime DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8;
-
+  `file_folder_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件夹名称',
+  `parent_folder_id` int(11) NULL DEFAULT 0 COMMENT '父文件夹ID',
+  `file_store_id` int(10) NULL DEFAULT NULL COMMENT '所属文件仓库ID',
+  `time` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 195 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `file_store`
+-- Table structure for file_store
 -- ----------------------------
 DROP TABLE IF EXISTS `file_store`;
-CREATE TABLE `file_store` (
+CREATE TABLE `file_store`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文件仓库ID',
-  `user_id` int(11) DEFAULT NULL COMMENT '主人ID',
-  `current_size` int(11) DEFAULT '0' COMMENT '当前容量（单位KB）',
-  `max_size` int(11) DEFAULT '1048576' COMMENT '最大容量（单位KB）',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8;
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '主人ID',
+  `current_size` int(11) NULL DEFAULT 0 COMMENT '当前容量（单位KB）',
+  `max_size` int(11) NULL DEFAULT 1048576 COMMENT '最大容量（单位KB）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 87 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `my_file`
+-- Table structure for my_file
 -- ----------------------------
 DROP TABLE IF EXISTS `my_file`;
-CREATE TABLE `my_file` (
+CREATE TABLE `my_file`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文件ID',
-  `my_file_name` varchar(255) DEFAULT NULL COMMENT '文件名',
-  `file_store_id` int(11) DEFAULT NULL COMMENT '文件仓库ID',
-  `my_file_path` varchar(255) DEFAULT '/' COMMENT '文件存储路径',
-  `download_time` int(11) DEFAULT '0' COMMENT '下载次数',
-  `upload_time` datetime DEFAULT NULL COMMENT '上传时间',
-  `parent_folder_id` int(11) DEFAULT NULL COMMENT '父文件夹ID',
-  `size` int(11) DEFAULT NULL COMMENT '文件大小',
-  `type` int(11) DEFAULT NULL COMMENT '文件类型',
-  `postfix` varchar(255) DEFAULT NULL COMMENT '文件后缀',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8;
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名',
+  `file_hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件哈希值',
+  `file_store_id` int(10) NULL DEFAULT NULL COMMENT '文件仓库ID',
+  `file_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '/' COMMENT '文件存储路径',
+  `download_num` int(11) NULL DEFAULT 0 COMMENT '下载次数',
+  `upload_time` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上传时间',
+  `parent_folder_id` int(11) NULL DEFAULT NULL COMMENT '父文件夹ID',
+  `size` int(11) NULL DEFAULT NULL COMMENT '文件大小',
+  `size_str` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件大小单位',
+  `type` int(11) NULL DEFAULT NULL COMMENT '文件类型',
+  `postfix` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件后缀',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 243 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `user`
+-- Table structure for share
+-- ----------------------------
+DROP TABLE IF EXISTS `share`;
+CREATE TABLE `share`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `file_id` int(11) NULL DEFAULT NULL,
+  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `open_id` varchar(255) DEFAULT NULL COMMENT '用户的openid',
-  `file_store_id` int(11) DEFAULT NULL COMMENT '文件仓库ID',
-  `user_name` varchar(50) DEFAULT NULL COMMENT '用户名',
-  `register_time` datetime DEFAULT NULL COMMENT '注册时间',
-  `image_path` varchar(255) DEFAULT '' COMMENT '头像地址',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8;
+CREATE TABLE `user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `open_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户的openid',
+  `file_store_id` int(10) NULL DEFAULT NULL COMMENT '文件仓库ID',
+  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `register_time` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `image_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '头像地址',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
